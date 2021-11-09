f36164fdbcb6b381fbc8fbd8d44d69ef09366fe6
Satont <satontworldwide@gmail.com>
2021-11-09 16:07:45 +0300

    Add expire feature

--- src/mod.ts
+++ src/mod.ts
@@ -1,20 +1,23 @@
-import { Client, StorageAdapter } from './deps.deno.ts'
+import { Client, StorageAdapter } from './deps.deno.ts';
 
 export class RedisAdapter<T> implements StorageAdapter<T> {
-  private redis: Client
+  private redis: Client;
+  private readonly ttl?: number;
 
-  constructor({
-    instance,
-  }: {
-    instance?: Client,
-  }) {
+  /**
+   * @constructor
+   * @param {opts} Constructor options
+   * @param {opts.ttl} ttl - Session time to life in SECONDS.
+   * @param {opts.instance} instance - Instance of redis.
+   */
+  constructor({ instance, ttl }: { instance?: Client; ttl?: number }) {
     if (instance) {
       this.redis = instance;
     } else {
       throw new Error('You should pass redis instance to constructor.');
     }
 
-    return this;
+    this.ttl = ttl;
   }
 
   async read(key: string) {
@@ -27,6 +30,9 @@ export class RedisAdapter<T> implements StorageAdapter<T> {
 
   async write(key: string, value: T) {
     await this.redis.set(key, JSON.stringify(value));
+    if (this.ttl) {
+      this.redis.expire(key, this.ttl);
+    }
   }
 
   async delete(key: string) {
