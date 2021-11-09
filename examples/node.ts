80c1fe5d06b62bc0dd3e91435503438d7499f405
Satont <42675886+Satont@users.noreply.github.com>
2021-11-09 16:08:43 +0300

    Add expire feature (#8)

--- examples/node.ts
+++ examples/node.ts
@@ -1,5 +1,5 @@
-import { Bot, Context, session, SessionFlavor } from "grammy";
-import { RedisAdapter } from "@satont/grammy-redis-storage";
+import { Bot, Context, session, SessionFlavor } from 'grammy';
+import { RedisAdapter } from '@satont/grammy-redis-storage';
 import IORedis from 'ioredis';
 
 interface SessionData {
@@ -7,24 +7,22 @@ interface SessionData {
 }
 type MyContext = Context & SessionFlavor<SessionData>;
 
-const redisInstance = new IORedis('redis://localhost:6379/0')
+const redisInstance = new IORedis('redis://localhost:6379/0');
 
 //create storage
-const storage = new RedisAdapter({ instance: redisInstance })
+const storage = new RedisAdapter({ instance: redisInstance, ttl: 10 });
 
 // Create bot and register session middleware
-const bot = new Bot<MyContext>("");
+const bot = new Bot<MyContext>('');
 bot.use(
   session({
     initial: () => ({ counter: 0 }),
     storage,
-  })
+  }),
 );
 
 // Register your usual middleware, and start the bot
-bot.command("stats", (ctx) =>
-  ctx.reply(`Already got ${ctx.session.counter} photos!`)
-);
-bot.on(":photo", (ctx) => ctx.session.counter++);
+bot.command('stats', (ctx) => ctx.reply(`Already got ${ctx.session.counter} photos!`));
+bot.on(':photo', (ctx) => ctx.session.counter++);
 
 bot.start();
