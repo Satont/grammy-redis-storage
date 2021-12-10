a218a00f0dce457cdca8bb1cc4ee23fca6d1f6cb
Satont <satontworldwide@gmail.com>
2021-12-10 18:01:05 +0300

    Use @satont/grammy-storage-utils for tests

--- __tests__/deno.ts
+++ __tests__/deno.ts
@@ -1,30 +1,11 @@
-import { Bot, Context, SessionFlavor, session } from 'https://deno.land/x/grammy@v1.3.4/mod.ts';
+import { session } from 'https://deno.land/x/grammy@v1.5.2/mod.ts';
 import { expect } from 'https://deno.land/x/expect@v0.2.9/mod.ts'
 import { RedisAdapter } from '../src/mod.ts';
 import { RedisMock } from './redisMock.ts'
+import { createBot, createMessage } from 'https://deno.land/x/grammy_storage_utils@1.0.1/mod.ts'
 
-interface SessionData {
-  pizzaCount: number;
-}
-
-interface StringSessionFlavor {
-  get session(): string;
-  set session(session: string | null | undefined);
-}
-
- 
 Deno.test('Pizza counter tests', async () => {
-  const bot = new Bot<Context & SessionFlavor<SessionData>>('fake-token', { 
-    botInfo: {
-      id: 42,
-      first_name: 'Test Bot',
-      is_bot: true,
-      username: 'bot',
-      can_join_groups: true,
-      can_read_all_group_messages: true,
-      supports_inline_queries: false,
-    },
-  });;
+  const bot = createBot()
 
   bot.use(session({
     initial: () => ({ pizzaCount: 0 }),
@@ -45,17 +26,7 @@ Deno.test('Pizza counter tests', async () => {
 })
 
 Deno.test('Simple string tests', async () => {
-  const bot = new Bot<Context & StringSessionFlavor>('fake-token', { 
-    botInfo: {
-      id: 42,
-      first_name: 'Test Bot',
-      is_bot: true,
-      username: 'bot',
-      can_join_groups: true,
-      can_read_all_group_messages: true,
-      supports_inline_queries: false,
-    },
-  });;
+  const bot = createBot(false)
 
   bot.use(session({
     initial: () => 'test',
@@ -73,26 +44,3 @@ Deno.test('Simple string tests', async () => {
   await bot.handleUpdate(createMessage(bot, 'first').update);
   await bot.handleUpdate(createMessage(bot, 'second').update);
 })
-
-function createMessage(bot: Bot<any>, text = 'Test Text') {
-  const createRandomNumber = () => Math.floor(Math.random() * (123456789 - 1) + 1);
-
-  const ctx = new Context({ 
-    update_id: createRandomNumber(), 
-    message: { 
-      text,
-      message_id: createRandomNumber(),
-      chat: { 
-        id: 1,
-        type: 'private',
-        first_name: 'Test User',
-      },
-      date: Date.now(),
-    },
-  }, 
-  bot.api, 
-  bot.botInfo
-  );
-
-  return ctx;
-}
